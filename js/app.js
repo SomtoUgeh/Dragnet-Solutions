b0e10887e64572fbeb3e5f716e65b5e08a2e8b8f
somtougeh <smugeh@gmail.com>
2018-05-11 14:36:58 +0100

    Commented JS for Firefox

--- js/app.js
+++ js/app.js
@@ -8,13 +8,13 @@ window.onscroll = function() {
   //   background();
 };
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("myBtn").style.display = "block";
-  } else {
-    document.getElementById("myBtn").style.display = "none";
-  }
-}
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     document.getElementById("myBtn").style.display = "block";
+//   } else {
+//     document.getElementById("myBtn").style.display = "none";
+//   }
+// }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
