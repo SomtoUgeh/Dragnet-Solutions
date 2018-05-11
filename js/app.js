2ac6b9f0113dbe5579159c5b09eeda91cf44940e
somtougeh <smugeh@gmail.com>
2018-05-11 09:41:08 +0100

    navigation links and social media added

--- js/app.js
+++ js/app.js
@@ -5,8 +5,7 @@ var sticky = header.offsetTop;
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {
   scrollFunction();
-  headerDown();
-  background();
+  //   background();
 };
 
 function scrollFunction() {
