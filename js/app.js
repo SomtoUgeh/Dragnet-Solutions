b6ef5a4c0814116a1c34a2442514890c4e74959f
somtougeh <smugeh@gmail.com>
2018-05-14 09:13:24 +0100

    UI Update

--- js/app.js
+++ js/app.js
@@ -5,16 +5,33 @@ var sticky = header.offsetTop;
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {
   scrollFunction();
+  headerDown();
   //   background();
 };
 
-// function scrollFunction() {
-//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//     document.getElementById("myBtn").style.display = "block";
-//   } else {
-//     document.getElementById("myBtn").style.display = "none";
-//   }
-// }
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+function headerDown() {
+  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+    header.classList.add("header_down");
+  } else {
+    header.classList.remove("header_down");
+  }
+}
+
+function linkChange() {
+  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
